c926b63912243c8aa3216762bf83ba4168caba78
Joe Jackson <cpmhjoe@gmail.com>
2019-11-01 13:14:59 -0400

    [#71] Add Migrations For Icons

--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,20 @@
 table! {
+    file_extensions (id) {
+        id -> Int4,
+        extension -> Varchar,
+        icon_mapping_id -> Nullable<Int4>,
+    }
+}
+
+table! {
+    file_names (id) {
+        id -> Int4,
+        name -> Varchar,
+        icon_mapping_id -> Nullable<Int4>,
+    }
+}
+
+table! {
     github_users (id) {
         id -> Int4,
         login -> Varchar,
@@ -7,6 +23,14 @@ table! {
         user_id -> Nullable<Int4>,
         created_at -> Timestamp,
         updated_at -> Timestamp,
+    }
+}
+
+table! {
+    icon_mappings (id) {
+        id -> Int4,
+        file_type -> Varchar,
+        image_file -> Varchar,
     }
 }
 
@@ -56,8 +80,14 @@ table! {
     }
 }
 
+joinable!(file_extensions -> icon_mappings (icon_mapping_id));
+joinable!(file_names -> icon_mappings (icon_mapping_id));
+
 allow_tables_to_appear_in_same_query!(
+    file_extensions,
+    file_names,
     github_users,
+    icon_mappings,
     pull_requests,
     reviews,
     users,
